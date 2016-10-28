fd63a723916c4ac7399745d53bc5ee6ed266ef88
Kevin A. Finley <kf.two.two@gmail.com>
2016-10-28 09:31:25 -0700

    add more to read me

---
+++ README.md
@@ -0,0 +1,14 @@
+1) use the console to check your code
+2)set you variable the use it to be greater or eqaul to users input  to count
+input: 3
+output: 1, 2, 3
+
+3) then use the modulus to 15 and use the console.log to check your work
+input: 15
+output: ping-pong
+
+4)Then do it for 3 and 5
+input:3
+output: ping  
+input: 5
+output: pong
--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,16 @@
+
+
+
 var i;
-for (i=1; i <= 15; i++) {
-
-} if (i % 15 == 0)
+for (i = 1; i<= 15; i++) {
+  console.log(i);
+  if (i % 15 == 0)
+  console.log("ping-pong");
+  else if (i % 5 ==0)
   console.log("pong");
-
-
-
-
-
-
-
-
-
+  else if  (i % 3== 0)
+  console.log("ping");
+}
 
 // $(document).ready(function(){
 //   $("#userInput").submit(event) {
