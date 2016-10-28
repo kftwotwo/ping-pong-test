8853691dbaf2b4587e2c40d58d0cc4b266a3526e
Kevin A. Finley <kf.two.two@gmail.com>
2016-10-28 14:35:11 -0700

    fix all logic except jquery

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,13 @@ var list = [];
 var pingPong = function(num) {
   for (i = 1; i<= num; i++) {
     if (i % 15 == 0) {
-      list[i].push("Ping Pong");
+      list.push("Ping Pong");
     } else if (i % 5 == 0) {
-     list[i].push("Pong");
+     list.push("Pong");
     } else if (i % 3 == 0) {
-     list[i].push("Ping");
+     list.push("Ping");
     } else
-     list[i];
+     list.push(i);
   }
   return list;
 }
