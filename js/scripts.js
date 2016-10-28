5c7c79ee75d965129ce2853bf406f2803b610e15
Kevin A. Finley <kf.two.two@gmail.com>
2016-10-28 13:33:26 -0700

    fix spelling errors in js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,12 @@
-var i;
 var list = [];
 
 var pingPong = function(num) {
   for (i = 1; i<= num; i++) {
     if (i % 15 == 0) {
-      lsit[i] = "Ping Pong";
+      list[i] = "Ping Pong";
     } else if (i % 5 == 0) {
      list[i] = "Ping";
-    } else if (i % 3== 0) {
+    } else if (i % 3 == 0) {
      list[i] = "Pong";
     } else
      list[i];
@@ -19,17 +18,17 @@ $(document).ready(function(){
   $("#userInput").submit(function(event){
     event.preventDefault();
     var input = $("#userInput").val();
-    var results = pingPong(num);
+    var results = pingPong(input);
     $("#output").empty();
     results.forEach(function(result) {
-      if (list == "Ping Pong") {
-        $("#output").text("<li>" + result + "</li>");
+      if (result == "Ping Pong") {
+        $("#output").append("<li>" + results + "</li>");
       } else if (result = "pong") {
-        $("#output").text("<li>" + result + "</li>");
+        $("#output").append("<li>" + results + "</li>");
       } else if (result = "ping") {
-        $("#output").text("<li>" + result + "</li>");
+        $("#output").append("<li>" + results + "</li>");
       } else {
-        $("#output").text("<li>" + result + "</li>");
+        $("#output").append("<li>" + results + "</li>");
       }
     })
   })
